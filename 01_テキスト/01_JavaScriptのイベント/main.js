285767543fcadb7701208775ad83a203721741f8
masan894 <MasanaMail762@gmail.com>
2022-10-07 17:27:22 +0900

    [3の倍数]を学習しました。

---
+++ 01_テキスト/01_JavaScriptのイベント/main.js
@@ -0,0 +1,17 @@
+// button要素とclickのイベント
+const button = document.getElementById("button")
+const alertMessage = function () {
+  alert("クリックしたよ")
+}
+button.onclick = alertMessage
+
+//以下を追加
+const inputText = document.getElementById("input-text")
+const inputDate = document.getElementById("input-date")
+
+const logValue = function (e) {
+  console.log(e.target.value)
+}
+
+inputText.oninput = logValue
+inputDate.oninput = logValue
